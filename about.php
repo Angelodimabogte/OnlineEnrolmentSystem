627084ae5aa7fd58776c636f80240014aaea8b70
Kris Jane Cabilan <cabilankrisjane@gmail.com>
2024-04-17 22:46:53 +0800

    first commit

---
+++ about.php
@@ -0,0 +1,35 @@
+   <!-- /.row -->
+          
+                     
+            <div class="row">
+                 
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3><b>  Mission and Vision</b>
+                           </h3>
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <p>
+                               <h1> VISION</h1>
+
+                               To be the leader and dominant provider of relevant globally recognized information technology-based education and related services in the global market.
+
+                                     
+
+                                   <h1>  MISSION</h1>
+
+                                   To provide a holistic, relevant, quality and globally recognized IT-based education in all levels and disciplines with the objective of producing professionals and leaders responsive to the needs of Science and the international community cognizant of the welfare and benefits of its men and women thereby realizing their potential as productive members of the society for the honor and glory of God Almighty.
+                                     
+
+                                  
+                            </p>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-6 -->
+            </div>
+           
---
+++ home.php
@@ -0,0 +1,69 @@
+ <!-- Map Column -->
+ <div class="row">
+            <div class="col-md-4">
+                <img width="100%" src="img/aclc.png">
+            </div>
+            <!-- Contact Details Column -->
+            <div class="col-md-8">
+                <h3>Contact Details</h3>
+                <p>
+                    ACLC College of Iriga City Inc. 
+                    <br>2nd Floor, Jasaca Building H1 San Miguel, Iriga City, Philippines<br>
+                </p>
+                <p><i class="fa fa-phone"></i> 
+                    <abbr title="Phone">P</abbr>: (054) 299-5884</p>
+                <p><i class="fa fa-envelope-o"></i> 
+                    <abbr title="Email">Email</abbr>: <a href="mailto:name@example.com">Admission@aclcirigaph.com</a>
+                </p>
+                <p><i class="fa fa-clock-o"></i> 
+                    <abbr title="Hours">H</abbr>: Monday - Saturday: 9:00 AM to 5:00 PM</p>
+                <ul class="list-unstyled list-inline list-social-icons">
+                    <li>
+                        <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-linkedin-square fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-twitter-square fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-google-plus-square fa-2x"></i></a>
+                    </li>
+                </ul>
+            </div>
+</div>
+    <!-- /.row -->
+          
+                     <br/>
+            <div class="row">
+                 
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                       <!--  <div class="panel-heading">
+                            <h3><b>  Mission and Vision</b>
+                           </h3>
+                        </div> -->
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <p>
+                               <h1> VISION</h1>
+
+                               To be the leader and dominant provider of relevant globally recognized information technology-based education and related services in the global market.
+
+                                     
+
+                                   <h1>  MISSION</h1>
+
+                                   To provide a holistic, relevant, quality and globally recognized IT-based education in all levels and disciplines with the objective of producing professionals and leaders responsive to the needs of Science and the international community cognizant of the welfare and benefits of its men and women thereby realizing their potential as productive members of the society for the honor and glory of God Almighty.
+
+                                   
+                                </p>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-6 -->
+            </div>
+           
